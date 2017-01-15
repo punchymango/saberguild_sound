4e9614c7789b6029c583e67206fde5fd379f48da
Darryl Parks <darrylcparks@gmail.com>
2017-01-14 17:15:33 -0800

    Add files via upload

--- scripts/sound.js
+++ scripts/sound.js
@@ -23,47 +23,52 @@ var musicPlayer = {
   },
 
   toggleRepeat : function() { //controls the repeat button
+    var rButton = document.getElementById('rButton');
     if (this.repeat) {
       rButton.style.backgroundColor = 'ivory';
     } else if (!this.repeat) {
-      rButton.style.backgroundColor = '#4200f7';
+      rButton.style.backgroundColor = '#849bff';
     }
     this.repeat = !this.repeat;
   },
 
   toggleDelay : function() {
+    var dButton = document.getElementById('dButton');
     if (this.delay) {
       dButton.style.backgroundColor = 'ivory';
     } else if (!this.delay) {
-      dButton.style.backgroundColor = '#4200f7';
+      dButton.style.backgroundColor = '#849bff';
     }
     this.delay = !this.delay;
   },
 
   select : function(type, id) {
+    var pButton = document.getElementById('pButton');
     var title = id.toUpperCase();
+    var playHead = document.getElementById('playhead');
     document.querySelector('audio').setAttribute('src', this.tracks[type][id]);
     document.getElementById('title').textContent = title;
     this.activeTrack = this.tracks[type][id];
     pButton.className = 'play';
-    playhead.style.marginLeft = '0px';
+    playHead.style.marginLeft = '0px';
   },
 
   playMusic : function() { //actually plays the track, and gets the timeline handling going
     var interval;
     var delay;
+    var pButton = document.getElementById('pButton');
     if (this.activeTrack === undefined) {
       return;
     }
 
     handlers.setupTimelineHandler();
-    if (music.paused) {
-      music.play();
+    if (musicPlayer.music.paused) {
+      musicPlayer.music.play();
       pButton.className = '';
 		  pButton.className = 'pause';
-		  interval = setInterval(display.showNumbers, 1000);
+		  interval = setInterval(display.showNumbers, 300);
     } else {
-      music.pause();
+      musicPlayer.music.pause();
 		  pButton.className = '';
 		  pButton.className = 'play';
 		  clearInterval(interval);
@@ -87,7 +92,6 @@ var handlers = {
      if (track === '' || track === 'fight') {
        return;
      }
-
      musicPlayer.select('fight', track);
      display.clearActives();
      display.makeActive(event.target);
@@ -108,20 +112,19 @@ var handlers = {
     musicPlayer.music.addEventListener('canplaythrough', function() {
       handlers.duration = musicPlayer.music.duration;
     }, false);
-  this.timeline.addEventListener("click", function (event) {
-	display.moveplayhead(event);
-	musicPlayer.music.currentTime = music.duration * handlers.clickPercent(event);
+  document.getElementById('timeline').addEventListener("click", function (event) {
+	  musicPlayer.music.currentTime = music.duration * handlers.clickPercent(event);
+  	display.moveplayhead(event);
 }, false);
   display.showNumbers();
  },
 
- playhead : document.getElementById('playhead'),
  duration : undefined,
- timeline : document.getElementById('timeline'),
-
   timeUpdate : function() {
-	var playPercent = 100 * (musicPlayer.music.currentTime / music.duration);
-	handlers.playhead.style.marginLeft = playPercent + "%";
+  var pButton = document.getElementById('pButton');
+  var playHead = document.getElementById('playhead');
+	var playPercent = 100 * (musicPlayer.music.currentTime / musicPlayer.music.duration);
+	playHead.style.marginLeft = playPercent + "%";
 	if (musicPlayer.music.currentTime == this.duration && musicPlayer.repeat === false) {
         pButton.className = "";
         pButton.className = "play";
@@ -133,15 +136,17 @@ var handlers = {
 
   playHandler : function() {
     var delay;
-    if (musicPlayer.delay && music.paused && music.currentTime === 0) {
-      delay = window.setTimeout(function(){musicPlayer.playMusic()}, 10000);
+    if (musicPlayer.delay && musicPlayer.music.paused && musicPlayer.music.currentTime === 0) {
+      delay = window.setTimeout(function(){musicPlayer.playMusic();}, 10000);
     } else {
       musicPlayer.playMusic();
     }
   },
 
   clickPercent: function(e) {
-	return (e.pageX - timeline.offsetLeft) / timeline.offsetWidth;
+    var timeline = document.getElementById('timeline');
+    var playerMargin = document.getElementById('player');
+	return (e.pageX - (timeline.offsetLeft + player.offsetLeft)) / timeline.offsetWidth;
 },
 
 
@@ -151,15 +156,16 @@ var handlers = {
 
 var display = {
   moveplayhead : function(e) {
+  var playHead = document.getElementById('playhead');
 	var newMargLeft = e.pageX - timeline.offsetLeft;
 	if (newMargLeft !== 0 && newMargLeft !== timeline.offsetWidth) {
-		playhead.style.marginLeft = newMargLeft + 'px';
+		playHead.style.marginLeft = newMargLeft + 'px';
 	}
 	if (newMargLeft ===  0) {
-		playhead.style.marginLeft = '0px';
+		playHead.style.marginLeft = '0px';
 	}
 	if (newMargLeft === timeline.offsetWidth) {
-		playhead.style.marginLeft = timeline.offsetWidth + 'px';
+		playHead.style.marginLeft = timeline.offsetWidth + 'px';
 	}
 
 },
@@ -169,10 +175,10 @@ var display = {
       track_duration.className = '';
       elapsed.className = '';
     }
-    var xMinutes = Math.floor(parseInt(music.currentTime) / 60);
-    var yMinutes = Math.floor(parseInt(music.duration) / 60);
-    var xSeconds = parseInt(music.currentTime);
-    var ySeconds = parseInt(music.duration);
+    var xMinutes = Math.floor(parseInt(musicPlayer.music.currentTime) / 60);
+    var yMinutes = Math.floor(parseInt(musicPlayer.music.duration) / 60);
+    var xSeconds = parseInt(musicPlayer.music.currentTime);
+    var ySeconds = parseInt(musicPlayer.music.duration);
     if (xMinutes > 0) {
       xSeconds = xSeconds - (xMinutes * 60);
     }
@@ -191,7 +197,7 @@ var display = {
   },
 
   makeActive : function(element) {
-    element.className += ' active_track';
+    element.className = 'track-heading bold lite-up active_track';
   },
 
   clearActives : function() {
@@ -200,17 +206,17 @@ var display = {
     var forceList = document.getElementById('force').childNodes;
     kataList.forEach(function(node){
       if (node.nodeName === 'P') {
-        node.className = 'track-heading bold lite-up';
+        node.className = 'track-heading bold lite-up inactive_track';
       }
     });
     fiteList.forEach(function(node){
       if (node.nodeName === 'P') {
-        node.className = 'track-heading bold lite-up';
+        node.className = 'track-heading bold lite-up inactive_track';
       }
     });
     forceList.forEach(function(node){
       if (node.nodeName === 'P') {
-        node.className = 'track-heading bold lite-up';
+        node.className = 'track-heading bold lite-up inactive_track';
       }
     });
   }
@@ -218,9 +224,6 @@ var display = {
 
 handlers.setupClickHandlers();
 
-/*musicPlayer.music.addEventListener('canplaythrough', function() {
-    handlers.duration = musicPlayer.music.duration;
-}, false);*/
 
 musicPlayer.music.addEventListener('canplay', function() {
     display.showNumbers();
